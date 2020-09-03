59d5aef8609d73a5ed159d852fb1a5967c193d7d
Andrei Stratanovich <andreistratanovich@gmail.com>
2020-09-03 06:02:45 +0300

    Updates

---
+++ precache-manifest.043a39783fcaf2de3a6d1a6d65f9e5eb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dbc7c165ea12e86d1c75903377eb6423",
+    "url": "/maps-univer/index.html"
+  },
+  {
+    "revision": "e99594f89df026d9a2ac",
+    "url": "/maps-univer/static/css/2.9edc93b8.chunk.css"
+  },
+  {
+    "revision": "9d98347c408a2b8fda45",
+    "url": "/maps-univer/static/css/main.d07f5ca7.chunk.css"
+  },
+  {
+    "revision": "e99594f89df026d9a2ac",
+    "url": "/maps-univer/static/js/2.5105db2e.chunk.js"
+  },
+  {
+    "revision": "56729d980da09961afe8d497a21eec12",
+    "url": "/maps-univer/static/js/2.5105db2e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d98347c408a2b8fda45",
+    "url": "/maps-univer/static/js/main.2df7ac7f.chunk.js"
+  },
+  {
+    "revision": "2b1e3fbdb715a9b23600",
+    "url": "/maps-univer/static/js/runtime-main.8bbce123.js"
+  }
+]);
